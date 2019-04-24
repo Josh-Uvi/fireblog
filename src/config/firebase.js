5a3bc3617e26685cfad29bd3a70438faaa5a4f91
Joshua Uvbiekpahor <40158394+Josh-Uvi@users.noreply.github.com>
2019-04-24 11:23:35 +0100

    Removed firebase details

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,12 +4,12 @@ import 'firebase/auth'
 
 // Initialize Firebase
 const config = {
-  apiKey: "AIzaSyAje6s5BVd6zcwiOnikejS4eWwgur9Fcw0",
-  authDomain: "fireblog-cc3bf.firebaseapp.com",
-  databaseURL: "https://fireblog-cc3bf.firebaseio.com",
-  projectId: "fireblog-cc3bf",
-  storageBucket: "fireblog-cc3bf.appspot.com",
-  messagingSenderId: "534336765517"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: ""
 };
 
 firebase.initializeApp(config);
