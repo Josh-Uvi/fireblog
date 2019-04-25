cd3ba7dd6a5054439e3b695de3301d3a573d999a
Joshua Uvbiekpahor <joshuauvbiekpahor@gmail.com>
2019-04-25 21:16:56 +0100

    second  commit

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,15 +1,15 @@
-import firebase from 'firebase/app'
-import 'firebase/firestore'
-import 'firebase/auth'
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
 
 // Initialize Firebase
 const config = {
-  apiKey: "AIzaSyAje6s5BVd6zcwiOnikejS4eWwgur9Fcw0",
-  authDomain: "fireblog-cc3bf.firebaseapp.com",
-  databaseURL: "https://fireblog-cc3bf.firebaseio.com",
-  projectId: "fireblog-cc3bf",
-  storageBucket: "fireblog-cc3bf.appspot.com",
-  messagingSenderId: "534336765517"
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: ''
 };
 
 firebase.initializeApp(config);
